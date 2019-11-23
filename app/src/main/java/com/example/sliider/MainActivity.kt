4bb6ef0bbee035232c25f243c7bd0ab621fbf4a9
Josh <onabanjotobi@gmail.com>
2019-11-23 13:07:40 +0000

    Added next / back button and new assets

--- app/src/main/java/com/example/sliider/MainActivity.kt
+++ app/src/main/java/com/example/sliider/MainActivity.kt
@@ -2,17 +2,72 @@ package com.example.sliider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.viewpager.widget.ViewPager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val adapter by lazy { SliderAdapter(this.applicationContext) }
+    private val adapter by lazy { SliderAdapter(this.applicationContext) }
+    private var currentPage = 0
+    private var lastPage = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         onboardingViewPager.adapter = adapter
+
+        nextButton.setOnClickListener {
+            onboardingViewPager.currentItem = currentPage + 1
+        }
+
+        previousButton.setOnClickListener {
+            onboardingViewPager.currentItem = currentPage - 1
+        }
+        onboardingViewPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                currentPage = position
+                when (position) {
+                    0 -> {
+                        nextButton.isEnabled = true
+                        previousButton.isEnabled = false
+                        previousButton.visibility = View.INVISIBLE
+
+                        nextButton.text = "Next"
+                        previousButton.text = ""
+                    }
+
+                    lastPage -> {
+                        nextButton.isEnabled = true
+                        previousButton.isEnabled = true
+                        previousButton.visibility = View.VISIBLE
+
+                        nextButton.text = "Finish"
+                        previousButton.text = "Back"
+                    }
+
+                    else -> {
+                        nextButton.isEnabled = true
+                        previousButton.isEnabled = true
+                        previousButton.visibility = View.VISIBLE
+
+                        nextButton.text = "Next"
+                        previousButton.text = "Back"
+                    }
+                }
+            }
+
+        })
+
+
     }
 }
--- app/src/main/java/com/example/sliider/SliderAdapter.kt
+++ app/src/main/java/com/example/sliider/SliderAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.sliider
 
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,7 +12,11 @@ class SliderAdapter(
     val context: Context
 ) : PagerAdapter() {
 
-    val slideImage = emptyList<Drawable>()
+    val slideImage = listOf(
+        R.drawable.ic_selfie,
+        R.drawable.ic_hooked,
+        R.drawable.ic_popular
+    )
 
     val slideHeader = listOf(
         "Lorem ipsum dolor.",
@@ -36,9 +39,10 @@ class SliderAdapter(
     }
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
-//        return super.instantiateItem(container, position)
         val layoutInflater : LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = layoutInflater.inflate(R.layout.slide_layout, container, false)
+
+        view.onboardingImage.setImageResource(slideImage[position])
 
         view.onboardingHeader.text = slideHeader[position]
         view.onboardingDescription.text = slideDescription[position]
@@ -49,7 +53,6 @@ class SliderAdapter(
 
     // Once last page has been reached it will stop instead of creating multiple slide (stops crash)
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-//        super.destroyItem(container, position, `object`)
         container.removeView(`object` as ConstraintLayout)
     }
 }
